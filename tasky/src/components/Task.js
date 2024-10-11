63a47713f763ed35254fd97994804535b8923e27
kelite12 <20108870@mail.wit.ie>
2024-10-11 14:24:54 +0100

    Delete a task

--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -8,6 +8,7 @@ function Task(props){
             <p>Due: {props.deadline}</p>
             <p className="style">{props.description}</p>
             <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
         </div>
     )
 }
